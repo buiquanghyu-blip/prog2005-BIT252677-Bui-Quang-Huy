e8c4ced39236c5534af9b44396ca04e9d93fa76f
buiquanghyu-blip <buiquanghyu@gmail.com>
2026-01-29 22:50:26 +0700

    Create Ex5.py

---
+++ chapter 1/Ex5.py
@@ -0,0 +1,14 @@
+name = "Nguyễn Thủy Hỏa"
+age = 18
+average_score = 8.5
+
+# Hiển thị kiểu dữ liệu của từng biến
+print("name:", name, "- Kiểu dữ liệu:", type(name))
+print("age:", age, "- Kiểu dữ liệu:", type(age))
+print("average_score:", average_score, "- Kiểu dữ liệu:", type(average_score))
+
+age_next_year = age + 1
+doubled_score = average_score * 2
+
+print("age_next_year:", age_next_year, "- Kiểu dữ liệu:", type(age_next_year))
+print("doubled_score:", doubled_score, "- Kiểu dữ liệu:", type(doubled_score))
