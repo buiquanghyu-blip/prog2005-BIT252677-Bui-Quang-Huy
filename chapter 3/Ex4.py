fb81f2e752487f35d1c1b91ff48ca287424071d0
buiquanghyu-blip <buiquanghyu@gmail.com>
2026-03-04 23:04:44 +0700

    Create Ex4.py

---
+++ chapter 3/Ex4.py
@@ -0,0 +1,12 @@
+numbers = [5, 2, 8, 5, 1, 3]
+
+print("Danh sách ban đầu:", numbers)
+
+numbers.sort()
+print("Sau khi sắp xếp tăng dần:", numbers)
+
+numbers.reverse()
+print("Sau khi đảo ngược:", numbers)
+
+count_5 = numbers.count(5)
+print("Số lần xuất hiện của 5:", count_5)
