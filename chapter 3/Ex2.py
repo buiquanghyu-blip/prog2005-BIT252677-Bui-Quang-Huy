253f33d59034a27e0773f2281b633b3ee0c6e035
buiquanghyu-blip <buiquanghyu@gmail.com>
2026-03-04 23:01:09 +0700

    Create Ex2.py

---
+++ chapter 3/Ex2.py
@@ -0,0 +1,9 @@
+friends = ["An", "Bình", "Chi"]
+
+print("Danh sách ban đầu:", friends)
+
+friends.append("Dũng")
+print("Sau khi thêm bạn mới:", friends)
+
+friends.pop(1)
+print("Sau khi xóa bạn ở vị trí thứ hai:", friends)
