a74a5d29f6a340344737714c7e718a5a7cd15df9
buiquanghyu-blip <buiquanghyu@gmail.com>
2026-03-04 23:19:02 +0700

    Create Ex8.py

---
+++ chapter 3/Ex8.py
@@ -0,0 +1,12 @@
+numbers = list(map(int, input("Nhập các số cách nhau bởi dấu cách: ").split()))
+
+found_number = None
+
+for num in numbers:
+    if num > 10:
+        found_number = num
+        break
+if found_number is not None:
+    print("Số đầu tiên lớn hơn 10 là:", found_number)
+else:
+    print("Không có số nào lớn hơn 10 trong danh sách.")
