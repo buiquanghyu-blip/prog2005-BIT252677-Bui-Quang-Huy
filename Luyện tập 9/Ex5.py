f17f091dd11dece44e49f182a5ceac793d2bbe4d
buiquanghyu-blip <buiquanghyu@gmail.com>
2026-03-16 17:24:02 +0700

    Create Ex5.py

---
+++ Luyện tập 9/Ex5.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self, id):
+        self._id = id   
+
+    @property
+    def id(self):
+        return self._id
+
+
+u = User(101)
+
+print("User ID:", u.id)
+
+u.id = 200
